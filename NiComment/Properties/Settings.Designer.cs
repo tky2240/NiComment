8762370d3c3cf50171ed1242058b3f67bfd308fc
伯文 堀内 <tky2240@gmail.com>
2022-07-19 21:54:38 +0900

    add default settings

--- NiComment/Properties/Settings.Designer.cs
+++ NiComment/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace NiComment.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("admin")]
         public string UserName {
             get {
                 return ((string)(this["UserName"]));
@@ -37,7 +37,7 @@ namespace NiComment.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("admin")]
         public string Password {
             get {
                 return ((string)(this["Password"]));
